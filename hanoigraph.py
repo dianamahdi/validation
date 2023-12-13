702dbce7a514d6d7fc03f35e3cf5e9be22a3af5b
diana <dianamahdi18@gmail.com>
2023-12-13 09:10:24 +0100

    hanoigraph updated

--- hanoigraph.py
+++ hanoigraph.py
@@ -1,27 +1,27 @@
 from collections import deque
 
-
 class HanoiConfig:
     def __init__(self, num_disks):
         self.num_disks = num_disks
-        self.start_state = (tuple(range(num_disks, 0, -1)), (), ())
-        self.end_state = ((), (), tuple(range(num_disks, 0, -1)))
-
+        # Utilisation de listes au lieu de tuples pour représenter les tours
+        self.start_state = [list(range(num_disks, 0, -1)), [], []]
+        self.end_state = [[], [], list(range(num_disks, 0, -1))]
 
 class HanoiRootedGraph(HanoiConfig):
     def __init__(self, num_disks):
         super().__init__(num_disks)
 
-    def get_neighbors(self, state):
+    def get_neighbors(self, config):
         neighbors = []
-        for i, start_tower in enumerate(state):
-            if start_tower:
-                for j, end_tower in enumerate(state):
+        for i, start_tower in enumerate(config):
+            if start_tower:  # Check if there is at least one disk on the tower
+                for j, end_tower in enumerate(config):
                     if i != j and (not end_tower or start_tower[-1] < end_tower[-1]):
-                        new_state = list(map(list, state))
-                        disk = new_state[i].pop()
-                        new_state[j].append(disk)
-                        neighbors.append(tuple(map(tuple, new_state)))
+                        # Effectuer une copie profonde pour créer un nouvel état
+                        new_config = [tower[:] for tower in config]
+                        disk = new_config[i].pop()  # Retirer le disque du sommet de la tour de départ
+                        new_config[j].append(disk)  # Ajouter le disque au sommet de la tour d'arrivée
+                        neighbors.append(new_config)
         return neighbors
 
     def bfs(self):
@@ -29,16 +29,18 @@ class HanoiRootedGraph(HanoiConfig):
         queue = deque([(self.start_state, [])])
 
         while queue:
-            current_state, path = queue.popleft()
-            if current_state == self.end_state:
-                return path + [current_state]
+            current_config, path = queue.popleft()
+            if current_config == self.end_state:
+                return path + [current_config]
 
-            visited.add(current_state)
-            for neighbor in self.get_neighbors(current_state):
-                if neighbor not in visited:
-                    queue.append((neighbor, path + [current_state]))
+            config_tuple = tuple(map(tuple, current_config))  # Convertir en tuple pour les sets
+            if config_tuple not in visited:
+                visited.add(config_tuple)
+                for neighbor in self.get_neighbors(current_config):
+                    neighbor_tuple = tuple(map(tuple, neighbor))  # Convertir en tuple pour les sets
+                    if neighbor_tuple not in visited:
+                        queue.append((neighbor, path + [current_config]))
         return None
-
 
 # Example usage:
 num_disks = 3
