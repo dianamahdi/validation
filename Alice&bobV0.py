cb67a2d7f1feba4b5b5cc7b9779d8892f1ae9794
diana <dianamahdi18@gmail.com>
2024-01-10 08:36:03 +0100

    Updated

---
+++ Alice&bobV0.py
@@ -0,0 +1,60 @@
+from souplang import *
+from algorithms import *
+from kernel import *
+import inspect
+
+
+class aliceBobConfig:
+    def __init__(self):
+        self.apc = 0
+        self.bpc = 0
+
+    def __hash__(self):
+        return int(hash(self.apc + self.bpc))
+
+    def __eq__(self, other):
+        return self.apc == other.apc & self.bpc == other.bpc
+
+    def __repr__(self):
+        return str(self.apc) + str(self.bpc)
+
+
+def aliceBob():
+    instanceConfCount = aliceBobConfig()
+    soup = Behavior_Soup(instanceConfCount)
+
+    # def aliceBobToInit(c):
+    #     c.apc = 0
+    #     c.bpc = 0
+
+    # soup.add("aliceBobToInit", lambda c: c.apc < maxi, aliceBobToInit)
+
+    def aToSc(c):
+        c.apc = 1
+
+    soup.add("aToSc", lambda c: c.apc == 0, aToSc)
+
+    def bToSc(c):
+        c.bpc = 1
+
+    soup.add("bToSc", lambda c: c.bpc == 0, bToSc)
+
+
+    def aToInit(c):
+        c.apc = 0
+
+    soup.add("aToInit", lambda c: c.apc == 1, aToInit)
+
+    def bToInit(c):
+        c.bpc = 0
+
+    soup.add("bToInit", lambda c: c.bpc == 1, bToInit)
+
+
+    # def aliceBobToSc(c):
+    #     c.apc = 1
+    #     c.bpc = 1
+
+    # soup.add("aliceBobToSc", lambda c: c.apc + c.bpc == maxi, aliceBobToSc)
+
+    return soup
