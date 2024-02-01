2df933fdc1d4bbd32474ebeeaef4864b1f7febc2
diana <dianamahdi18@gmail.com>
2024-02-01 22:00:42 +0100

    Updated

--- Algorithms.py
+++ Algorithms.py
@@ -40,7 +40,74 @@ def find_cycle(graph, initial, end):
                 visited.append(n)
     return False
 
+def bfs_safe(rg):
+    visited = set()
+    queue = deque()
+    at_start = True
 
+    while at_start or len(queue) > 0:
+        if at_start:
+            neighbors = [rg.initial()]
+            at_start = False
+        else:
+            current = queue.popleft()
+            neighbors = rg.next(current)
+
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            if rg.is_accepting(neighbor):
+                return False
+
+            queue.append(neighbor)
+            visited.add(neighbor)
+
+    return True
+
+
+#Test
+# Sample graph represented as an adjacency list
+class Graph:
+    def __init__(self):
+        self.graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F'],
+            'D': [],
+            'E': ['F'],
+            'F': []
+        }
+
+    def initial(self):
+        return 'A'  # Change the initial node as needed
+
+    def roots(self):
+        return ['A']
+
+    def neighbors(self, node):
+        return self.graph.get(node, [])
+
+    def is_accepting(self, node):
+        return node == 'F'
+
+if __name__ == '__main__':
+    graph = Graph()
+
+    # Test BFS function
+    print("Testing BFS:")
+    result, visited_nodes = bfs(graph, lambda x: x == 'F')
+    print("Result:", result)
+    print("Visited Nodes:", visited_nodes)
+    print()
+
+    # Test bfs_safe function
+    print("Testing bfs_safe:")
+    is_safe = bfs_safe(graph)
+    print("Is Safe:", is_safe)
+
+
+'''
 def bfs2(rg, query):
     visited = set()
     queue = deque()
@@ -63,7 +130,7 @@ def bfs2(rg, query):
                     queue.append(neighbor)
                     visited.add(neighbor)
     return None, visited
-
+'''
 
 
 
